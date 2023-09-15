993e1cd0344c0db290117de51a76455c3e620a48
Harshit Bhat <78542114+harshitbhat@users.noreply.github.com>
2023-09-15 10:05:14 +0530

    Chapter 3.1

--- 03. Common Programming Concepts/variables-and-mutability/src/main.rs
+++ 03. Common Programming Concepts/variables-and-mutability/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+
+    let y = 10;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("The value of y in the inner scop is {y}");
+    }
+
+    println!("The value of y is: {y}");
+
+    let spaces = "   ";
+    println!("The value of spaces is: '{spaces}'");
+
+    let spaces = spaces.len();
+    println!("The value of spaces is: {spaces}");
 }
