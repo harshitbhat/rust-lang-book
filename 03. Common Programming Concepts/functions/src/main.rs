0bbb044abe4d16d8056583d33b744262b4fb01b0
Harshit Bhat <78542114+harshitbhat@users.noreply.github.com>
2023-09-20 11:15:09 +0530

    Chapter 3.3

--- 03. Common Programming Concepts/functions/src/main.rs
+++ 03. Common Programming Concepts/functions/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    another_function();
+    another_function_one(5);
+    print_labeled_measurement(42, 'h');
+    println!("The value of x is {}", five());
+    println!("The value of x is {}", plus_one(five()));
+}
+
+fn another_function() {
+    println!("Another function.")
+}
+
+fn another_function_one(x: i32) {
+    println!("The value of x is : {x}")
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is : {value} {unit_label}")
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
