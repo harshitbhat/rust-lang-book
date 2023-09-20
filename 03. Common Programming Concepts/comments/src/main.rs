e2734dea11229b9c2b35703e866dcafab1bb607a
Harshit Bhat <78542114+harshitbhat@users.noreply.github.com>
2023-09-20 11:17:41 +0530

    Chapter 3.4

--- 03. Common Programming Concepts/comments/src/main.rs
+++ 03. Common Programming Concepts/comments/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
+    // hello, world
+
+    // So we’re doing something complicated here, long enough that we need
+    // multiple lines of comments to do it! Whew! Hopefully, this comment will
+    // explain what’s going on.
+
+    /*
+    
+    Multi
+    lines comments
+     */
 }
